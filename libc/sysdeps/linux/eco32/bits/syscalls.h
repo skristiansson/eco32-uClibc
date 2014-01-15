2cf1e89a15707fa5c152fc5f22b51572fb5f5689
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-01-15 11:55:43 +0200

    eco32: initial port
    Incomplete but compiles.
    
    Signed-off-by: Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>

---
+++ libc/sysdeps/linux/eco32/bits/syscalls.h
@@ -0,0 +1,71 @@
+#ifndef _BITS_SYSCALLS_H
+#define _BITS_SYSCALLS_H
+#ifndef _SYSCALL_H
+# error "Never use <bits/syscalls.h> directly; include <sys/syscall.h> instead."
+#endif
+
+#ifndef __ASSEMBLER__
+
+#include <errno.h>
+
+/*
+ * GCC can't handle a constraint on $25 directly, hence the extra ori.
+ * I think this might be related to that $25 is a fixed register,
+ * but it needs investigation in gcc.
+ */
+#define INTERNAL_SYSCALL_NCS(name, err, nr, args...)			\
+(__extension__								\
+({									\
+	register long __ret __asm__("$2");				\
+	long _scno = name;						\
+	LOAD_ARGS_##nr(args);						\
+	__asm__ __volatile__("ori $25,%1,0\n"				\
+			     "trap"					\
+			     : "=r"(__ret)				\
+			     : "r"(_scno) ASM_ARGS_##nr			\
+			     : ASM_CLOBBERS_##nr			\
+			       "$10", "$11", "$12", "$13",		\
+			       "$14", "$15");				\
+	__ret;								\
+})									\
+)
+#define INTERNAL_SYSCALL_ERROR_P(val, err)	\
+	((unsigned long)(val) >= (unsigned long)(-125))
+
+#define LOAD_ARGS_0() do { } while(0)
+#define ASM_ARGS_0
+#define ASM_CLOBBERS_0	"$4", ASM_CLOBBERS_1
+#define LOAD_ARGS_1(a1)				\
+	register long _a1 __asm__("$4") = (long)(a1);	\
+	LOAD_ARGS_0()
+#define ASM_ARGS_1	ASM_ARGS_0, "r"(_a1)
+#define ASM_CLOBBERS_1	"$5", ASM_CLOBBERS_2
+#define LOAD_ARGS_2(a1, a2)				\
+	register long _a2 __asm__("$5") = (long)(a2);	\
+	LOAD_ARGS_1(a1)
+#define ASM_ARGS_2	ASM_ARGS_1, "r"(_a2)
+#define ASM_CLOBBERS_2	"$6", ASM_CLOBBERS_3
+#define LOAD_ARGS_3(a1, a2, a3)				\
+	register long _a3 __asm__("$6") = (long)(a3);	\
+	LOAD_ARGS_2(a1, a2)
+#define ASM_ARGS_3	ASM_ARGS_2, "r"(_a3)
+#define ASM_CLOBBERS_3	"$7", ASM_CLOBBERS_4
+#define LOAD_ARGS_4(a1, a2, a3, a4)			\
+	register long _a4 __asm__("$7") = (long)(a4);	\
+	LOAD_ARGS_3(a1, a2, a3)
+#define ASM_ARGS_4	ASM_ARGS_3, "r"(_a4)
+#define ASM_CLOBBERS_4	"$8", ASM_CLOBBERS_5
+/* The rest aren't real function parameter registers */
+#define LOAD_ARGS_5(a1, a2, a3, a4, a5)			\
+	register long _a5 __asm__("$8") = (long)(a5);	\
+	LOAD_ARGS_4(a1, a2, a3, a4)
+#define ASM_ARGS_5	ASM_ARGS_4, "r"(_a5)
+#define ASM_CLOBBERS_5	"$9", ASM_CLOBBERS_6
+#define LOAD_ARGS_6(a1, a2, a3, a4, a5, a6)		\
+	register long _a6 __asm__("$9") = (long)(a6);	\
+	LOAD_ARGS_5(a1, a2, a3, a4, a5)
+#define ASM_ARGS_6	ASM_ARGS_5, "r"(_a6)
+#define ASM_CLOBBERS_6
+
+#endif /* __ASSEMBLER__ */
+#endif /* _BITS_SYSCALLS_H */
---
+++ libc/sysdeps/linux/eco32/jmpbuf-offsets.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2000-2006 Erik Andersen <andersen@uclibc.org>
+ *
+ * Licensed under the LGPL v2.1, see the file COPYING.LIB in this tarball.
+ */
+
+#include <features.h>
+
+#define JB_REGS		0
+#define JB_PC		32
+#define JB_SP		36
+#define JB_FP		40
